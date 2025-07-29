3f02f4f82c76107e6b1638e05c3dc8f42901dd24
Dharmendra <yaduvanshi.dhanno@gmail.com>
2025-07-29 21:21:34 +0530

    update: work.ts file

--- constant/works.ts
+++ constant/works.ts
@@ -6,21 +6,27 @@ export const works=[
         description: 'Blog App using Nextjs, Tailwind css and Postgress Blog App using Nextjs, Tailwind css and Postgress Blog App using Nextjs, Tailwind css and Postgress Blog App using Nextjs, Tailwind css and Postgress'
     },
     {
-        title: 'Bits and Byte Blog',
-        link: 'https://bitsandbyte-blog.vercel.app/',
-        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753705798/Screenshot_2025-07-28_175830_eqrlth.png',
-        description: 'Blog App using Nextjs, Tailwind css and Postgress'
+        title: 'Prompt Dekho',
+        link: 'https://prompt-dekho.vercel.app',
+        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753734415/prompt-dekho_axqgce.png',
+        description: 'A Webiste that lest you share your prompts with others. Classify promopts on basis on tags.'
     },
     {
-        title: 'Bits and Byte Blog',
-        link: 'https://bitsandbyte-blog.vercel.app/',
-        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753705798/Screenshot_2025-07-28_175830_eqrlth.png',
-        description: 'Blog App using Nextjs, Tailwind css and Postgress'
+        title: 'Veg Delight',
+        link: 'https://veg-delight.vercel.app/',
+        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753733902/veg-delight_gaujrh.png',
+        description: 'A Landing Page for a restaraunt, simple, sleek, modern and responsive.'
     },
     {
-        title: 'Bits and Byte Blog',
-        link: 'https://bitsandbyte-blog.vercel.app/',
-        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753705798/Screenshot_2025-07-28_175830_eqrlth.png',
-        description: 'Blog App using Nextjs, Tailwind css and Postgress'
+        title: 'Nike',
+        link: 'https://dxy-nikeui.vercel.app/',
+        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753734279/nike-react-ui_dx8djx.png',
+        description: 'React and Tailwind based modern and responsive landing page'
+    },
+    {
+        title: 'Zentry Redefine Gaming',
+        link: 'https://dxy-zentry.vercel.app/',
+        cover: 'https://res.cloudinary.com/dofvsgdbf/image/upload/v1753734282/redefine-gaming_cininw.png',
+        description: 'Motion based awwwards website for gaming website'
     }
 ]
